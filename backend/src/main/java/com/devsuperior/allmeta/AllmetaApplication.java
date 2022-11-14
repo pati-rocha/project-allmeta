58184230d271e7cb76e546ace58444b177ccaafa
Pati Rocha <patirocha.dev@gmail.com>
2022-11-14 16:16:55 -0300

    project created

---
+++ backend/src/main/java/com/devsuperior/allmeta/AllmetaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.allmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/allmeta/AllmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.allmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
